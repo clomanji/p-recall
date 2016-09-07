be1f8e032b6e3c8807c94827fa7d5b591b5719bb
francesc <francesc.guillen@gmail.com>
2016-09-07 13:21:31 +0200

    setup test

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject p-recall "0.1.0-SNAPSHOT"
+(defproject clomanji/p-recall "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.227"]]
+                 [org.clojure/clojurescript "1.9.227"]
+                 [clomanji/sm2 "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-doo "0.1.6"]
@@ -12,4 +13,21 @@
 
   :auto {"test" {:file-pattern #"\.(clj|cljs|cljc|edn)$"}}
 
-  :clean-targets ^{:protect false} ["resources/public/js" "target" "out"])
+  :clean-targets ^{:protect false} ["resources/public/js" "target" "out"]
+
+  :cljsbuild {:builds [{:id "min"
+                        :source-paths ["src"]
+                        :compiler {:main p-recall.core
+                                   :output-to "resources/deploy/js/compiled/app.js"
+                                   :optimizations :advanced
+                                   :closure-defines {goog.DEBUG false}
+                                   :pretty-print false}}
+
+                       {:id "unit-tests"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-to "out/unit_tests.js"
+                                   :main 'p-recall.unit-tests
+                                   :target :nodejs
+                                   :optimizations :none}}]}
+
+  )
---
+++ test/p_recall/unit_tests.cljs
@@ -0,0 +1,7 @@
+ (ns p-recall.unit-tests
+   (:require
+     [doo.runner :refer-macros [doo-tests]]
+     [p-recall.core-test]))
+
+ (doo-tests
+   'p-recall.core-test)
